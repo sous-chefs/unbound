d37730d38d5b5b6a6a5b6e638e58cf04bbd19266
James Snow <james.snow@rd.io>
2014-01-22 13:26:55 -0800

    Bump version number.

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer_email "cookbooks@housepub.org"
 license          "Apache 2.0"
 description      "Manages unbound DNS resolver"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.1.0"
+version          "0.2.0"
 
 recipe "unbound::default", "Installs unbound and sets up configuration files"
 recipe "unbound::chroot", "Sets up the chroot environment if chroot attribute is enabled"
