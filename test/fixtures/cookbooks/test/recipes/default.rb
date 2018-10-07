1e1aa0808cc255fb29604c58c3e090117db9b0f9
Dan Webb <dan.webb@damacus.io>
2018-10-07 20:33:06 +0100

    Let's write an example in the tests

--- test/fixtures/cookbooks/test/recipes/default.rb
+++ test/fixtures/cookbooks/test/recipes/default.rb
@@ -1,2 +1,12 @@
 # frozen_string_literal: true
-include_recipe 'unbound::default'
+
+unbound_install 'unbound'
+
+unbound_configure 'bar' do
+  local_zone 'id' => 'int_example_com',
+             'ns' => [{ 'int.example.com' => '127.0.0.1' }],
+             'host' => [{ 'www.int.example.com' => '10.1.1.200' }]
+  forward_zone 'forward1' => [{ 'forward.example.com' => '10.1.1.201' }],
+               'forward2' => [{ 'forward2.example.com' => ['10.1.1.202', '10.1.1.203'] }],
+               'forward3' => [{ 'forward3.example.com' => 'ns1.none' }]
+end
