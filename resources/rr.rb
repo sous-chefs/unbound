b2eed2f4ab050ae115fb230ec3b12db04d15c32c
Tim Smith <tsmith@chef.io>
2019-07-26 01:01:52 -0700

    Use chef style comment headers (#39)
    auto-corrected with cookstyle
    
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- resources/rr.rb
+++ resources/rr.rb
@@ -1,8 +1,8 @@
 #
-# Cookbook Name:: unbound
+# Cookbook:: unbound
 # Resource:: rr
 #
-# Copyright 2011, Joshua Timberman
+# Copyright:: 2011, Joshua Timberman
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
