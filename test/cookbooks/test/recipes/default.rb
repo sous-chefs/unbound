e1b0667022cf0a8b6c7a18333d17a8f1d33ade62
Benjamin M. Hughes <bmhughes@bmhughes.co.uk>
2022-03-04 15:16:57 +0000

    Initial test kitchen update

--- test/cookbooks/test/recipes/default.rb
+++ test/cookbooks/test/recipes/default.rb
@@ -2,4 +2,23 @@ include_recipe 'unbound::default'
 
 unbound_config 'unbound' do
   config_file '/etc/unbound/unbound-test.conf'
+  config({
+    server: {
+      verbosity: 1,
+      interface: [
+        '10.42.0.1',
+        '10.42.0.1@853',
+        '2001:470:1881::1',
+        '2001:470:1881::1@853',
+        '127.0.0.1',
+        '127.0.0.1@853',
+        '::1',
+        '::1@853',
+      ],
+    },
+  })
+end
+
+unbound_config_forward_zone 'test.zone' do
+  forward_addr %w(1.1.1.1 8.8.8.8)
 end
