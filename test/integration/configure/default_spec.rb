81c1e90ad378c042b302a3275118b2591da6ddfe
Benjamin M. Hughes <bmhughes@bmhughes.co.uk>
2022-03-06 19:16:18 +0000

    Add port check to Kitchen inspec test

--- test/integration/configure/default_spec.rb
+++
@@ -1,6 +0,0 @@
-# frozen_string_literal: true
-describe service 'unbound' do
-  it { should be_installed }
-  it { should be_enabled }
-  it { should be_running }
-end
--- test/integration/default/default_spec.rb
+++ test/integration/default/default_spec.rb
@@ -1,6 +1,15 @@
-# frozen_string_literal: true
 describe service 'unbound' do
   it { should be_installed }
   it { should be_enabled }
   it { should be_running }
+end
+
+describe port(53) do
+  it { should be_listening }
+  its('processes') { should include 'unbound' }
+end
+
+describe port(853) do
+  it { should be_listening }
+  its('processes') { should include 'unbound' }
 end
